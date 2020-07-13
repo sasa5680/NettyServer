0d89fdfe2da9cb908c2d190fb26fb8a9b1429829
sasa5680 <sasa5680@naver.com>
2020-07-13 22:38:32 +0900

    First commit
    boo ya

---
+++ src/main/java/Main.java
@@ -0,0 +1,13 @@
+import com.messageClass.Test;
+import com.messageClass.Test.test;
+
+public class Main {
+	
+	public static void main(String args[]) {
+		
+	
+		test t = Test.test.newBuilder().setUsername("fa").setPassword("22").build();
+		
+	}
+
+}
